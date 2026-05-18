c77913cf1f718d515f44b524ececccf8195b1aa0
FloFranz <florian.franz86@gmx.de>
2026-05-18 16:34:09 +0200

    update setup script

--- src/setup.R
+++ src/setup.R
@@ -17,19 +17,25 @@
 if (!file.exists(paste('data')) |
     (!file.exists(paste('data/raw_data')) |
      (!file.exists(paste('data/raw_data/forest_inventory')) |
-      (!file.exists(paste('data/raw_data/pc_leafoff_2024')) |
-       (!file.exists(paste('data/processed_data')) |
-        (!file.exists(paste('data/processed_data/forest_inventory')) |
-         (!file.exists(paste('data/metadata'))
-          )))))))
+      (!file.exists(paste('data/raw_data/pc_leafon_2023')) |
+       (!file.exists(paste('data/raw_data/pc_leafoff_2024')) |
+        (!file.exists(paste('data/processed_data')) |
+         (!file.exists(paste('data/processed_data/forest_inventory')) |
+          (!file.exists(paste('data/processed_data/cropped_chms_plots')) |
+           (!file.exists(paste('data/processed_data/metrics')) |
+            (!file.exists(paste('data/metadata'))
+             ))))))))))
   {
   
   dir.create('data')
   dir.create('data/raw_data')
   dir.create('data/raw_data/forest_inventory')
+  dir.create('data/raw_data/pc_leafon_2023')
   dir.create('data/raw_data/pc_leafoff_2024')
   dir.create('data/processed_data')
   dir.create('data/processed_data/forest_inventory')
+  dir.create('data/processed_data/cropped_chms_plots')
+  dir.create('data/processed_data/metrics')
   dir.create('data/metadata')
   
 } else {
